e22cd9e8c62de479048fbd28fcd0865126a2aac1
rirsoft <120116471+rirsoft@users.noreply.github.com>
2024-07-21 13:11:11 +0530

    fix splashscreen

--- src/url.js
+++ src/url.js
@@ -1,1 +1,1 @@
-{"url":"https://cdn.jsdelivr.net/gh/rirsoft/74b01839765460b970403984f4d40645@1/src/script.min.js"}
+{"url":"https://cdn.jsdelivr.net/gh/rirsoft/74b01839765460b970403984f4d40645@4/src/script.min.js"}
